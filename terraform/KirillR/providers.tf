7c911ba9e8f8277278fc3bdd098229a23f7b783d
Kirill Ivanov <kradzanovskij@gmail.com>
2020-04-19 19:02:06 +0000

    Update providers.tf

--- terraform/KirillR/providers.tf
+++ terraform/KirillR/providers.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region                  = "us-east-1"
 }
-data "terraform_remote_state" "aws" {
-  backend = "s3"
-  config = {
+terraform {
+  backend "s3" {
     bucket = "nbiryukov-tf-state"
     key    = "kirillR/terraform.tfstate"
     region = "us-east-1"
