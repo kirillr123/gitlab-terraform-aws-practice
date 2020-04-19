e1e235fcf19c0fff97f2a9a9969251d8564400d5
kirillr123 <wannabefree232@gmail.com>
2020-04-19 21:38:10 +0300

    remote fix

--- terraform/KirillR/providers.tf
+++ terraform/KirillR/providers.tf
@@ -2,12 +2,11 @@ provider "aws" {
   region                  = "us-east-1"
   profile                 = "default"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "tfstate-bucket-123"
-    key    = "terraformhw/terraform.tfstate"
+data "terraform_remote_state" "aws" {
+  backend = "s3"
+  config = {
+    bucket = "nbiryukov-tf-state"
+    key    = "kirillR/terraform.tfstate"
     region = "us-east-1"
   }
-
 }
