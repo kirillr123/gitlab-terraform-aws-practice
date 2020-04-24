0781587f8b065fb2237899e592a14b8b8e225d1b
kirillr123 <wannabefree232@gmail.com>
2020-04-24 22:42:20 +0300

    Runner config

--- terraform/KirillR/compute.tf
+++ terraform/KirillR/compute.tf
@@ -2,9 +2,6 @@ resource "aws_key_pair" "default" {
   key_name = "default"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgrd1rwl9sEqf2IWlRVwe1WfGyiJusnVf5J7cZWNSftlnPyNiNBZPeThJnyoUBXDCOtD2uhzrMY7qwRhLg+VKL5flZpWdONvp1EV2k6vP+Rx1b9N8KbPc/OsUAFgjcGRWVpO6g4ArUMw7h07XARxAu0Jz3vbEO9/9QpVsGa29IhstwklGOvlM1fPZHZSa3dlg/j+Q3Ottmy5u/2qDoM5NVOM0YlPXsOqLBPkt3OlCfdFEAbAkTWjC+CxKrFVGq1T7AnchZxrXxcdsRm3SovqkwpP/SOKy/Ih5531/3SSxs099R6c/j6UkvuyEXoehqZZqm7JXl11BC2e6+uy4YHOesw== rsa-key-20200419"
 }
-data "template_file" "user_data" {
-template = "${file("init.sh")}"
-}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -25,6 +22,7 @@ resource "aws_instance" "aws_instance" {
   instance_type = "t2.micro"
   associate_public_ip_address = true
   key_name = "default"
+  user_data = file("init.sh")
 }
 
 output "name" {
--- terraform/KirillR/init.sh
+++ terraform/KirillR/init.sh
@@ -3,29 +3,32 @@ sudo su
 yum -y update
 yum -y install git
 
-curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | sudo bash
-sudo yum -y install gitlab-runner
-
-
-
-git clone https://github.com/CsteerDevops1/wiki-framework.git
+#clone git repo
+git clone --single-branch --branch develop https://github.com/CsteerDevops1/wiki-framework.git
 cd /wiki-framework/
-git checkout develop
 
-
+#install docker
 amazon-linux-extras install docker
 systemctl start docker
 systemctl enable docker
+
+#install compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+#install and set up runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | sudo bash
+sudo yum -y install gitlab-runner
 
 sudo gitlab-runner register -n \
   --url https://gitlab.com/ \
   --registration-token E-Uxw5qFUR7_RDPdLi4Y \
   --executor shell \
-  --description "My Runner"
+  --description "Kirill-runner"
 
 sudo usermod -aG docker gitlab-runner
 sudo -u gitlab-runner -H docker info
-sudo -u gitlab-runner -H docker info
 
-chmod a+x deploy.sh
-./deploy.sh project_dir
+
